3b82bb8aa39a2f8f8e2c89fc63067a1ccc8d7cb1
Jakub Ryška <ryskajakub@seznam.cz>
2016-04-09 12:33:41 +0200

    comment info

--- public/index.php
+++ public/index.php
@@ -269,6 +269,7 @@
 									<p><a href="mailto:<?php i("contact_email") ?>"><?php i("contact_email") ?></a></p>
 								</div>
 								<div class="col-md-3">
+<!--
 									<h3><?php i("subscribe_h3"); ?></h3>
 									<p> 
 
@@ -281,6 +282,7 @@
 	</div>
 </form>
 </p>
+-->
 								</div>
 								<div class="col-md-3">
 									<h3><?php i("contact_organizer_h3"); ?></h3>
